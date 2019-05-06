6139ade5b5f3ebe56d85d8d81b2cab39836293de
Alex Råsten <alexander.rasten@afaforsakring.se>
2019-05-06 22:13:24 +0200

    moved stuff

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "./api"
+import "github.com/gin-gonic/gin"
 
 func main() {
-	api.Start()
+	start()
+}
+
+func start() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.GET("/pong", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "ping",
+		})
+	})
+	r.GET("/tracks/:id", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"id":     c.Params.ByName("id"),
+			"name":   "Suzuka",
+			"length": "1400",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080
 }
