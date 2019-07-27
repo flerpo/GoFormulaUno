a43d1e4ece16cc33aec505d69c15d46d8c84e3d8
Alex Råsten <alexander.rasten@afaforsakring.se>
2019-07-27 14:05:26 +0200

    new web angular 8

--- web/formulaWeb/src/app/app.module.ts
+++ web/formulaWeb/src/app/app.module.ts
@@ -2,12 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { TracksComponent } from './tracks/tracks.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TracksComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
