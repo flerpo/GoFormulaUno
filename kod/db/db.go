ca3a3b25fad9db34aab5780e2e8ab8705997050c
flerpo <alex.rasten@gmail.com>
2019-10-06 19:49:49 +0200

    gah

--- kod/db/db.go
+++ kod/db/db.go
@@ -2,7 +2,8 @@ package db
 
 import (
 	"github.com/flerpo/GoFormulaUno/kod/models"
-
+	"os"
+	"fmt"
 	"github.com/jinzhu/gorm"
 	//for mysql
 	_ "github.com/jinzhu/gorm/dialects/mysql"
@@ -24,9 +25,12 @@ func init() {
 //GetConnection a db connection
 func GetConnection() *gorm.DB {
 	var err error
+	var sqlpass = os.Getenv("SQLPASS")
+	var sqlserver = os.Getenv("SQLSERVER")
+	fmt.Println(sqlserver)
 	//db, err = gorm.Open("mssql", "sqlserver://sa:!_ucKy-!uKe@localhost:1433?database=labb")
-	//db, err := gorm.Open("mysql", "formula:sofarfrompassword@tcp(172.17.0.2:3306)/formula?charset=utf8&parseTime=True")
-	db, err := gorm.Open("mysql", "formula:sofarfrompassword@tcp(0.0.0.0:3306)/formula?charset=utf8&parseTime=True")
+	//db, err := gorm.Open("mysql", "formula:sofarfrompassword@tcp(172.17.0.2:3306)/formula?charset=utf8&pa	rseTime=True")
+	db, err := gorm.Open("mysql", "formula:" + sqlpass + "@tcp(" + sqlserver + ")/formula?charset=utf8&parseTime=True")
 
 	if err != nil {
 		panic(err)
